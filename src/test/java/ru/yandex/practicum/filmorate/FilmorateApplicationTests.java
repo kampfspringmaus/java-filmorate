ade9af3ccb8d3aba2c01169022750b2f059d0959
Pavel Antipov <enrique_up@mail.ru>
2026-05-17 16:21:10 +0300

    все тесты сделаны рабочими

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate;
 
 import com.google.gson.*;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
@@ -18,174 +19,100 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.util.List;
 
-
+@Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class FilmorateApplicationTests {
-  /*    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    private String getBaseUrl() {
-        return "http://localhost:" + port;
-    }*/
 
     private static final String BASE = "http://localhost:8080";
     private static HttpClient client;
-    Gson gson = new GsonBuilder()
-            .registerTypeAdapter(LocalDate.class, (JsonSerializer<LocalDate>) (src, typeOfSrc, context) ->
-                    new JsonPrimitive(src.toString()))
-            .registerTypeAdapter(LocalDate.class, (JsonDeserializer<LocalDate>) (json, typeOfT, context) ->
-                    LocalDate.parse(json.getAsString()))
-            .setPrettyPrinting()
-            .create();
+    Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, (JsonSerializer<LocalDate>) (src, typeOfSrc, context) -> new JsonPrimitive(src.toString())).registerTypeAdapter(LocalDate.class, (JsonDeserializer<LocalDate>) (json, typeOfT, context) -> LocalDate.parse(json.getAsString())).setPrettyPrinting().create();
 
     @BeforeAll
     static void beforeAll() throws Exception {
-        client = HttpClient.newBuilder()
-                .connectTimeout(Duration.ofSeconds(5))
-                .build();
+        client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(5)).build();
     }
 
     @Test
     @Order(1)
     void createFilmIsSuccessful() {
-        Film film1 = Film.builder()
-                .name("Миска")
-                .description("Фильм о миске и том, что было дальше")
-                .releaseDate(LocalDate.of(1970, 01, 01))
-                .duration(45)
-                .build();
+        Film film1 = Film.builder().name("Миска").description("Фильм о миске и том, что было дальше").releaseDate(LocalDate.of(1970, 01, 01)).duration(45).build();
         //Gson gson = new Gson();
 
 
         String filmBody = gson.toJson(film1);
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/films"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(filmBody))
-                .build();
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(BASE + "/films")).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(filmBody)).build();
         try {
-            HttpResponse<String> response = client.send(req,
-                    HttpResponse.BodyHandlers.ofString());
-            Assertions.assertEquals(200, response.statusCode(), "Создание фильма с валидными" +
-                    "параметрами должно проходить успешно");
+            HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
+            Assertions.assertEquals(200, response.statusCode(), "Создание фильма с валидными" + "параметрами должно проходить успешно");
 
         } catch (IOException | InterruptedException e) {
             System.out.println("createFilmIsSuccessful" + e.getMessage() + e.toString());
         }
     }
 
-
     @Test
     @Order(2)
     void createUserIsSuccessful() {
-        User user1 = User.builder()
-                .email("p.antipov@yandex.practicum")
-                .login("p.antipov")
-                .name("Pavlik")
-                .birthday(LocalDate.of(1989, 03, 11))
-                .build();
+        User user1 = User.builder().email("p.antipov@yandex.practicum").login("p.antipov").name("Pavlik").birthday(LocalDate.of(1989, 03, 11)).build();
         String userBody = gson.toJson(user1);
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/users"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(userBody))
-                .build();
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(BASE + "/users")).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(userBody)).build();
         try {
-            HttpResponse<String> response = client.send(req,
-                    HttpResponse.BodyHandlers.ofString());
-            Assertions.assertEquals(200, response.statusCode(), "Создание пользователя с валидными" +
-                    "параметрами должно проходить успешно");
+            HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
+            Assertions.assertEquals(200, response.statusCode(), "Создание пользователя с валидными" + "параметрами должно проходить успешно");
         } catch (IOException | InterruptedException e) {
             System.out.println("createUserIsSuccessful" + e.getMessage());
         }
     }
 
-   /* @Test
+    @Test
     @Order(3)
     void getFilmsReturn1Film() {
-        HttpRequest req2 = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/films"))
-                .GET()
-                .build();
+        HttpRequest req2 = HttpRequest.newBuilder().uri(URI.create(BASE + "/films")).GET().build();
 
         try {
-            HttpResponse<String> response2 = client.send(req2,
-                    HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response2 = client.send(req2, HttpResponse.BodyHandlers.ofString());
             JsonArray filmArray = JsonParser.parseString(response2.body()).getAsJsonArray();
-            List<Film> filmList = gson.fromJson(filmArray, List.class);
-            Assertions.assertEquals(200, response2.statusCode(), "Запрос списка " +
-                    "пользователей должен возвращать код 200");
-            Assertions.assertEquals("Миска", filmList.get(0).getName(),
-                    "У первого элемента массива название фильма - Миска");
+            List<Film> filmList = gson.fromJson(filmArray, new ListOfFilmsTypeToken().getType());
+            Assertions.assertEquals(200, response2.statusCode(), "Запрос списка " + "пользователей должен возвращать код 200");
+            Assertions.assertEquals("Миска", filmList.get(0).getName(), "У первого элемента массива название фильма - Миска");
         } catch (IOException | InterruptedException e) {
             System.out.println("getFilmsReturn1Film" + e.getMessage());
         }
-    }*/
+    }
 
-/*    @Test
+    @Test
     @Order(4)
     void getUsersReturn2Users() {
-        User user2 = User.builder()
-                .email("p.antipov@yandex.pathca")
-                .login("p.antipovs")
-                .name("Paul")
-                .birthday(LocalDate.of(1989, 07, 21))
-                .build();
+        User user2 = User.builder().email("p.antipov@yandex.pathca").login("p.antipovs").name("Paul").birthday(LocalDate.of(1989, 07, 21)).build();
         String userBody = gson.toJson(user2);
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/users"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(userBody))
-                .build();
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(BASE + "/users")).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(userBody)).build();
         try {
-            HttpResponse<String> response = client.send(req,
-                    HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
         } catch (IOException | InterruptedException e) {
             System.out.println("getUsersReturn2Users " + e.getMessage());
         }
-        HttpRequest req2 = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/users"))
-                .GET()
-                .build();
+        HttpRequest req2 = HttpRequest.newBuilder().uri(URI.create(BASE + "/users")).GET().build();
 
         try {
-            HttpResponse<String> response2 = client.send(req2,
-                    HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response2 = client.send(req2, HttpResponse.BodyHandlers.ofString());
             JsonArray userArray = JsonParser.parseString(response2.body()).getAsJsonArray();
-            List<User> userList = gson.fromJson(userArray, List.class);
-            Assertions.assertEquals(200, response2.statusCode(), "Запрос списка " +
-                    "пользователей должен возвращать код 200");
-            Assertions.assertEquals("p.antipov@yandex.pathca", userList.get(1).getEmail(),
-                    "У второго элемента массива должен email p.antipov@yandex.pathca");
+            List<User> userList = gson.fromJson(userArray, new ListOfUsersTypeToken().getType());
+            Assertions.assertEquals(200, response2.statusCode(), "Запрос списка " + "пользователей должен возвращать код 200");
+            Assertions.assertEquals("p.antipov@yandex.pathca", userList.get(0).getEmail(), "У первого элемента массива должен email p.antipov@yandex.pathca");
 
         } catch (IOException | InterruptedException e) {
             System.out.println("getUsersReturn2Users " + e.getMessage());
         }
-    }*/
+    }
 
     @Test
     void updateNonExistentFilmReturns500() {
-        Film film1 = Film.builder()
-                .id(1000)
-                .name("Миска")
-                .description("Фильм о миске и том, что было дальше")
-                .releaseDate(LocalDate.now())
-                .duration(45)
-                .build();
+        Film film1 = Film.builder().id(1000).name("Миска").description("Фильм о миске и том, что было дальше").releaseDate(LocalDate.now()).duration(45).build();
         String filmBody = gson.toJson(film1);
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/films"))
-                .header("Content-Type", "application/json")
-                .PUT(HttpRequest.BodyPublishers.ofString(filmBody))
-                .build();
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(BASE + "/films")).header("Content-Type", "application/json").PUT(HttpRequest.BodyPublishers.ofString(filmBody)).build();
         try {
-            HttpResponse<String> response = client.send(req,
-                    HttpResponse.BodyHandlers.ofString());
-            Assertions.assertEquals(500, response.statusCode(), "Обновление несуществующего фильма" +
-                    " должно возвращать код 500");
+            HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
+            Assertions.assertEquals(500, response.statusCode(), "Обновление несуществующего фильма" + " должно возвращать код 500");
 
         } catch (IOException | InterruptedException e) {
             System.out.println("updateNonExistentFilmReturns500" + e.getMessage());
@@ -194,24 +121,12 @@ class FilmorateApplicationTests {
 
     @Test
     void updateNonExistentUserReturns500() {
-        User user1 = User.builder()
-                .id(1000)
-                .email("p.antipov@yandex.practicum")
-                .login("p.antipov")
-                .name("Pavlik")
-                .birthday(LocalDate.of(1989, 03, 11))
-                .build();
+        User user1 = User.builder().id(1000).email("p.antipov@yandex.practicum").login("p.antipov").name("Pavlik").birthday(LocalDate.of(1989, 03, 11)).build();
         String userBody = gson.toJson(user1);
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/users"))
-                .header("Content-Type", "application/json")
-                .PUT(HttpRequest.BodyPublishers.ofString(userBody))
-                .build();
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(BASE + "/users")).header("Content-Type", "application/json").PUT(HttpRequest.BodyPublishers.ofString(userBody)).build();
         try {
-            HttpResponse<String> response = client.send(req,
-                    HttpResponse.BodyHandlers.ofString());
-            Assertions.assertEquals(500, response.statusCode(), "Обновление несуществующего пользователя" +
-                    " должно возвращать код 500");
+            HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
+            Assertions.assertEquals(500, response.statusCode(), "Обновление несуществующего пользователя" + " должно возвращать код 500");
         } catch (IOException | InterruptedException e) {
             System.out.println("updateNonExistentUserReturns500" + e.getMessage());
         }
@@ -220,23 +135,12 @@ class FilmorateApplicationTests {
 
     @Test
     void createFilmWithReleaseYear1870Returns500() {
-        Film film1 = Film.builder()
-                .name("Миска")
-                .description("Фильм о миске и том, что было дальше")
-                .releaseDate(LocalDate.of(1870, 01, 01))
-                .duration(45)
-                .build();
+        Film film1 = Film.builder().name("Миска").description("Фильм о миске и том, что было дальше").releaseDate(LocalDate.of(1870, 01, 01)).duration(45).build();
         String filmBody = gson.toJson(film1);
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/films"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(filmBody))
-                .build();
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(BASE + "/films")).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(filmBody)).build();
         try {
-            HttpResponse<String> response = client.send(req,
-                    HttpResponse.BodyHandlers.ofString());
-            Assertions.assertEquals(500, response.statusCode(), "Создание фильма " +
-                    "с датой выхода 1870 год должно возвращать код 500");
+            HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
+            Assertions.assertEquals(500, response.statusCode(), "Создание фильма " + "с датой выхода 1870 год должно возвращать код 500");
 
         } catch (IOException | InterruptedException e) {
             System.out.println("createFilmWithReleaseYear1870Returns500" + e.getMessage());
@@ -245,24 +149,12 @@ class FilmorateApplicationTests {
 
     @Test
     void createUserWithWrongEmailReturns500() {
-        User user1 = User.builder()
-                .id(1000)
-                .email("p.antipov")
-                .login("p.antipov")
-                .name("Pavlik")
-                .birthday(LocalDate.of(1989, 03, 11))
-                .build();
+        User user1 = User.builder().id(1000).email("p.antipov").login("p.antipov").name("Pavlik").birthday(LocalDate.of(1989, 03, 11)).build();
         String userBody = gson.toJson(user1);
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/users"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(userBody))
-                .build();
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(BASE + "/users")).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(userBody)).build();
         try {
-            HttpResponse<String> response = client.send(req,
-                    HttpResponse.BodyHandlers.ofString());
-            Assertions.assertEquals(500, response.statusCode(), "Создание пользователя " +
-                    "с невалидным email должно возвращать код 500");
+            HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
+            Assertions.assertEquals(500, response.statusCode(), "Создание пользователя " + "с невалидным email должно возвращать код 500");
         } catch (IOException | InterruptedException e) {
             System.out.println("createUserWithWrongEmailReturns500" + e.getMessage());
         }
@@ -271,25 +163,13 @@ class FilmorateApplicationTests {
     @Test
     void postRequestWithNoBodyReturns400() {
         String reqBody = gson.toJson("");
-        HttpRequest reqFilms = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/films"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(reqBody))
-                .build();
-        HttpRequest reqUsers = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/users"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(reqBody))
-                .build();
+        HttpRequest reqFilms = HttpRequest.newBuilder().uri(URI.create(BASE + "/films")).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(reqBody)).build();
+        HttpRequest reqUsers = HttpRequest.newBuilder().uri(URI.create(BASE + "/users")).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(reqBody)).build();
         try {
-            HttpResponse<String> responseFilms = client.send(reqFilms,
-                    HttpResponse.BodyHandlers.ofString());
-            HttpResponse<String> responseUsers = client.send(reqUsers,
-                    HttpResponse.BodyHandlers.ofString());
-            Assertions.assertEquals(400, responseFilms.statusCode(), "При пустом запросе " +
-                    "должен возвращаться код 400\"Bad Request\"");
-            Assertions.assertEquals(400, responseUsers.statusCode(), "При пустом запросе " +
-                    "должен возвращаться код 400\"Bad Request\"");
+            HttpResponse<String> responseFilms = client.send(reqFilms, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> responseUsers = client.send(reqUsers, HttpResponse.BodyHandlers.ofString());
+            Assertions.assertEquals(400, responseFilms.statusCode(), "При пустом запросе " + "должен возвращаться код 400\"Bad Request\"");
+            Assertions.assertEquals(400, responseUsers.statusCode(), "При пустом запросе " + "должен возвращаться код 400\"Bad Request\"");
         } catch (IOException | InterruptedException e) {
 
             System.out.println("postRequestWithNoBodyReturns400 " + e.getMessage());
---
+++ src/test/java/ru/yandex/practicum/filmorate/ListOfFilmsTypeToken.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.filmorate;
+
+import com.google.gson.reflect.TypeToken;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.List;
+
+public class ListOfFilmsTypeToken extends TypeToken<List<Film>> {
+}
